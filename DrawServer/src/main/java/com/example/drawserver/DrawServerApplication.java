d6225e649ad85b0e0a28fe9c640a36dd585aa9d9
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-27 16:53:49 +0900

    ✨ :: 프로젝트 생성

---
+++ DrawServer/src/main/java/com/example/drawserver/DrawServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.drawserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawServerApplication.class, args);
+	}
+
+}
---
+++ DrawServer/src/test/java/com/example/drawserver/DrawServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
