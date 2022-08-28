009443cc73e33a65b5c9beb1bbe2666ef98eae78
rlaisqls <rlaisqls@gmail.com>
2022-08-28 10:11:46 +0900

    📝 :: DefaultImage

---
+++ src/main/java/com/project/draw/global/image/DefaultImage.java
@@ -0,0 +1,8 @@
+package com.project.draw.global.image;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DefaultImage {
+    public static final String USER_PROFILE_IMAGE = "";
+}
