d44dfee27c66f3dc6df4273858d9f6675726e0f4
rlaisqls <rlaisqls@gmail.com>
2022-08-28 10:49:05 +0900

    📝 :: UserNotFoundException
    .

---
+++ src/main/java/com/project/draw/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.project.draw.domain.user.exception;
+
+import com.project.draw.global.error.exception.BusinessException;
+import com.project.draw.global.error.exception.ErrorCode;
+
+public class UserNotFoundException extends BusinessException {
+    public static final BusinessException EXCEPTION = new UserNotFoundException();
+    private UserNotFoundException(){
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}
