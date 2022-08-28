9d44bb011d291de95b1fedcebd3e7b2081101fbd
rlaisqls <rlaisqls@gmail.com>
2022-08-28 10:35:28 +0900

    📝 :: QueryMyInfoService

---
+++ src/main/java/com/project/draw/domain/user/service/QueryMyInfoService.java
@@ -0,0 +1,27 @@
+package com.project.draw.domain.user.service;
+
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.facade.UserFacade;
+import com.project.draw.domain.user.presentation.dto.response.QueryUserInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class QueryMyInfoService {
+
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public QueryUserInfoResponse execute(){
+
+        User user = userFacade.getCurrentUser();
+
+        return QueryUserInfoResponse
+                .builder()
+                .name(user.getName())
+                .profileImageUrl(user.getProfileImageUrl())
+                .build();
+    }
+}
