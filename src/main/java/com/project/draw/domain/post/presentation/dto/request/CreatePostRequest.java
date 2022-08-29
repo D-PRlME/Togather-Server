f6030ec60c512a21fb55bd5b6129b1d32612185b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-29 21:41:43 +0900

    ✨ :: 게시글 생성 Api 추가

---
+++ src/main/java/com/project/draw/domain/post/presentation/dto/request/CreatePostRequest.java
@@ -0,0 +1,24 @@
+package com.project.draw.domain.post.presentation.dto.request;
+
+import com.project.draw.domain.post.domain.Tag;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class CreatePostRequest {
+
+    @Size(min = 1, max = 30, message = "제목은 최소 1자 ~ 최대 30자 내외입니다.")
+    private String title;
+
+    @Size(min = 1, max = 1000, message = "내용은 최소 1자 ~ 최대 1000자 내외입니다.")
+    private String content;
+
+    private List<Tag> tags;
+
+    private String link;
+}
