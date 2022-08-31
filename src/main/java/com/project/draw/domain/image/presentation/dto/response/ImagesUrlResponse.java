25943fa40dd4f6b4d1e2f033ea6c9d30be8c8224
rlaisqls <rlaisqls@gmail.com>
2022-08-31 22:17:31 +0900

    📝 :: ImagesUrlResponse

---
+++ src/main/java/com/project/draw/domain/image/presentation/dto/response/ImagesUrlResponse.java
@@ -0,0 +1,13 @@
+package com.project.draw.domain.image.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class ImagesUrlResponse {
+
+    List<String> ImagesUrl;
+}
