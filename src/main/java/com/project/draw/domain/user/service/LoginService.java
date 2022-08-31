3b5bfa2b7bee8b2c23ad97ad2a8799348e3ac6c4
rlaisqls <rlaisqls@gmail.com>
2022-08-31 22:36:18 +0900

    ♻️ :: Facade 메서드로 대체

--- src/main/java/com/project/draw/domain/user/service/LoginService.java
+++ src/main/java/com/project/draw/domain/user/service/LoginService.java
@@ -2,16 +2,14 @@ package com.project.draw.domain.user.service;
 
 import com.project.draw.domain.auth.presentation.dto.TokenResponse;
 import com.project.draw.domain.user.domain.User;
-import com.project.draw.domain.user.domain.repository.UserRepository;
 import com.project.draw.domain.user.exception.PasswordMismatchException;
-import com.project.draw.domain.user.exception.UserNotFoundException;
+import com.project.draw.domain.user.facade.UserFacade;
 import com.project.draw.domain.user.presentation.dto.request.LoginRequest;
 import com.project.draw.global.security.jwt.JwtProperties;
 import com.project.draw.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -19,19 +17,17 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class LoginService {
 
-    private final UserRepository userRepository;
+    private final UserFacade userFacade;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final JwtProperties jwtProperties;
 
-    @Transactional
     public TokenResponse execute(LoginRequest request) {
 
         String accountId = request.getAccountId();
         String password = request.getPassword();
 
-        User user = userRepository.findByAccountId(accountId)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        User user = userFacade.getUserByAccountId(accountId);
 
         if(!passwordEncoder.matches(password, user.getPassword()))
             throw PasswordMismatchException.EXCEPTION;
