83b5818130115c59b8a2bea7e217f1fbdc1a9e56
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-02 14:47:50 +0900

    📝 :: CodeRequest

---
+++ src/main/java/com/project/draw/domain/auth/presentation/dto/request/CodeRequest.java
@@ -0,0 +1,12 @@
+package com.project.draw.domain.auth.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CodeRequest {
+
+    private String code;
+
+}
