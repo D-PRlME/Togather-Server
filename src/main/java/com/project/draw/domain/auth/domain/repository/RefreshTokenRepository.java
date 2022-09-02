07778f570fe7704bf9766639a5f1b27595d4833b
rlaisqls <rlaisqls@gmail.com>
2022-09-02 17:44:41 +0900

    ♻️ :: findByEmail 삭제

--- src/main/java/com/project/draw/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/draw/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findByToken(String refreshToken);
-    RefreshToken findByEmail(String email);
 }
