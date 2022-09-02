e855facd2e35f1a966c9d3288ebc4e1db453c060
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-02 14:46:42 +0900

    📝 :: TokenResponse(Feign)
    구글 액세스 토큰 받는 용

---
+++ src/main/java/com/project/draw/infrastructure/feign/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.project.draw.infrastructure.feign.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+
+}
