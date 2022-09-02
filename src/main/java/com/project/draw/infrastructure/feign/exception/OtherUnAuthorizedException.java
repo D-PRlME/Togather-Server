140bafe9b130d99f5cdf16a6fe9a637a178c151b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-02 14:45:12 +0900

    📝 :: OtherUnAuthorizedException

---
+++ src/main/java/com/project/draw/infrastructure/feign/exception/OtherUnAuthorizedException.java
@@ -0,0 +1,16 @@
+package com.project.draw.infrastructure.feign.exception;
+
+
+import com.project.draw.global.error.exception.BusinessException;
+import com.project.draw.global.error.exception.ErrorCode;
+
+public class OtherUnAuthorizedException extends BusinessException {
+
+    public static final BusinessException EXCEPTION =
+            new OtherUnAuthorizedException();
+
+    private OtherUnAuthorizedException() {
+        super(ErrorCode.OTHER_UNAUTHORIZED);
+    }
+
+}
