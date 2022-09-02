91e2458d6d7e60dc83684990a545ac26c08c7853
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-02 14:50:43 +0900

    📝 :: GoogleAuthService
    회원가입 로직까지 넣음

---
+++ src/main/java/com/project/draw/domain/auth/service/GoogleAuthService.java
@@ -0,0 +1,82 @@
+package com.project.draw.domain.auth.service;
+
+import com.project.draw.domain.auth.domain.RefreshToken;
+import com.project.draw.domain.auth.domain.repository.RefreshTokenRepository;
+import com.project.draw.domain.auth.presentation.dto.request.CodeRequest;
+import com.project.draw.domain.auth.presentation.dto.response.TokenResponse;
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.domain.repository.UserRepository;
+import com.project.draw.global.property.AuthProperties;
+import com.project.draw.global.security.jwt.JwtProperties;
+import com.project.draw.global.security.jwt.JwtTokenProvider;
+import com.project.draw.infrastructure.feign.client.GoogleAuth;
+import com.project.draw.infrastructure.feign.client.GoogleInfo;
+import com.project.draw.infrastructure.feign.dto.request.GoogleCodeRequest;
+import com.project.draw.infrastructure.feign.dto.response.GoogleInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class GoogleAuthService {
+
+    private final GoogleAuth googleAuth;
+    private final GoogleInfo googleInfo;
+    private final AuthProperties authProperties;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtProperties jwtProperties;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public TokenResponse execute(CodeRequest request) {
+        String accessToken = googleAuth.googleAuth(
+                GoogleCodeRequest.builder()
+                        .code(URLDecoder.decode(request.getCode(), StandardCharsets.UTF_8))
+                        .clientId(authProperties.getClientId())
+                        .clientSecret(authProperties.getClientSecret())
+                        .redirectUri(authProperties.getRedirectUrl())
+                        .build()
+        ).getAccessToken();
+
+        GoogleInfoResponse googleInfoResponse = googleInfo.googleInfo(accessToken);
+
+        String email = googleInfoResponse.getEmail();
+        String name = googleInfoResponse.getName();
+
+        String refreshToken = jwtTokenProvider.createRefreshToken(email);
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .email(email)
+                        .token(refreshToken)
+                        .expiration(jwtProperties.getRefreshExp())
+                        .build()
+        );
+        createUser(email, name);
+
+        return TokenResponse
+                .builder()
+                .accessToken(jwtTokenProvider.createAccessToken(email))
+                .refreshToken(refreshToken)
+                .expiredAt(LocalDateTime.now().plusSeconds(jwtProperties.getAccessExp()))
+                .build();
+    }
+
+    private void createUser(String email, String name) {
+        if (userRepository.findByEmail(email).isEmpty()) {
+            userRepository.save(
+                    User.builder()
+                            .email(email)
+                            .name(name)
+                            .build());
+        }
+    }
+}
