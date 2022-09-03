264c3e1153907021e695c8bef8cfd628399f1a3e
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-03 13:41:37 +0900

    📝 :: QueryGoogleAuthLinkService

---
+++ src/main/java/com/project/draw/global/util/auth/service/QueryGoogleAuthLinkService.java
@@ -0,0 +1,22 @@
+package com.project.draw.global.util.auth.service;
+
+import com.project.draw.global.properties.AuthProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class QueryGoogleAuthLinkService {
+
+    private static final String url = "%s?client_id=%s&redirect_uri=%s&response_type=code"
+            + "&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
+
+    private final AuthProperties authProperties;
+
+    public String execute() {
+        return String.format(url, //이 형식으로 아래 것들을 짜준다 ~
+                authProperties.getBaseUrl(),
+                authProperties.getClientId(),
+                authProperties.getRedirectUrl());
+    }
+}
