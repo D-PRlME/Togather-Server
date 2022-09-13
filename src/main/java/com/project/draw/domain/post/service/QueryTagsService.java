295ae525d92838ff47415bca3c5682e2501532b2
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-13 19:06:45 +0900

    📝 :: QueryTagsService

---
+++ src/main/java/com/project/draw/domain/post/service/QueryTagsService.java
@@ -0,0 +1,23 @@
+package com.project.draw.domain.post.service;
+
+import com.project.draw.domain.post.domain.Tag;
+import com.project.draw.domain.post.presentation.dto.response.TagListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class QueryTagsService {
+
+
+    public TagListResponse execute(){
+    return new TagListResponse(
+        Arrays
+                .stream(Tag.class.getEnumConstants())
+                .collect(Collectors.toList())
+            );
+    }
+}
