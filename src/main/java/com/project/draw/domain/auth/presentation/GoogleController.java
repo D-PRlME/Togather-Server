b2a75bd7861e7dbef2ebf864c63a14dc20a6504c
rlaisqls <rlaisqls@gmail.com>
2022-09-13 22:10:49 +0900

    ⚡ :: redirectUrl 매핑

--- src/main/java/com/project/draw/domain/auth/presentation/GoogleController.java
+++ src/main/java/com/project/draw/domain/auth/presentation/GoogleController.java
@@ -3,25 +3,25 @@ package com.project.draw.domain.auth.presentation;
 import com.project.draw.domain.auth.presentation.dto.response.TokenResponse;
 import com.project.draw.domain.auth.service.GoogleAuthService;
 import com.project.draw.global.util.auth.service.QueryGoogleAuthLinkService;
-import com.project.draw.infrastructure.feign.dto.request.CodeRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/google")
 @RestController
 public class GoogleController {
 
     private final GoogleAuthService googleAuthService;
     private final QueryGoogleAuthLinkService queryGoogleAuthLinkService;
 
-    @GetMapping("/auth")
+    @GetMapping("/google/auth")
     public String queryGoogleAuthLink() {
         return queryGoogleAuthLinkService.execute();
     }
 
-    @PostMapping("/auth")
-    public TokenResponse googleAuthLogin(@RequestBody CodeRequest codeRequest) {
-        return googleAuthService.execute(codeRequest);
+    @GetMapping("/receiveCode")
+    public TokenResponse googleAuthLogin(@RequestParam("code") String code) {
+        return googleAuthService.execute(code);
     }
 }
