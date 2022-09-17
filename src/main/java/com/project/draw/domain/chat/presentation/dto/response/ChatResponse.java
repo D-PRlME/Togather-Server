9285fe2625ac02dd4e3c9e0cd00ef94f088e7375
rlaisqls <rlaisqls@gmail.com>
2022-09-17 21:04:37 +0900

    📝 :: ChatResponse

---
+++ src/main/java/com/project/draw/domain/chat/presentation/dto/response/ChatResponse.java
@@ -0,0 +1,30 @@
+package com.project.draw.domain.chat.presentation.dto.response;
+
+import com.project.draw.domain.chat.domain.Chat;
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.presentation.dto.response.UserResponse;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public class ChatResponse {
+
+    private Long roomId;
+    private UserResponse user;
+    private LocalDateTime createdAt;
+    private String message;
+
+    public static ChatResponse of(Chat chat) {
+
+        User user = chat.getUser();
+
+        return ChatResponse
+                .builder()
+                .roomId(chat.getRoom().getId())
+                .user(UserResponse.of(user))
+                .message(chat.getMessage())
+                .createdAt(chat.getCreatedAt())
+                .build();
+    }
+}
