efe165131e92031b71d025cb2f4fefa3d1e13b02
rlaisqls <rlaisqls@gmail.com>
2022-09-17 21:06:15 +0900

    ⚡ :: ProjectDefaultImage

--- src/main/java/com/project/draw/global/image/DefaultImage.java
+++ src/main/java/com/project/draw/global/image/DefaultImage.java
@@ -5,4 +5,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DefaultImage {
     public static final String USER_PROFILE_IMAGE = "https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/tmpimg.png";
+    public static final String PROJECT_DEFAULT_IMAGE = "https://myawsbucketswg1214.s3.amazonaws.com/myawsbucketswg1214/tmpimg.png";
 }
