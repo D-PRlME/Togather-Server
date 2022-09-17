6e8d4c2a1a320da2032b5b0188d32b47c95da072
rlaisqls <rlaisqls@gmail.com>
2022-09-17 17:58:56 +0900

    📝 :: RoomType

---
+++ src/main/java/com/project/draw/domain/chat/domain/enums/RoomType.java
@@ -0,0 +1,13 @@
+package com.project.draw.domain.chat.domain.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RoomType {
+
+    PROJECT,
+    INDIVIDUAL
+}
