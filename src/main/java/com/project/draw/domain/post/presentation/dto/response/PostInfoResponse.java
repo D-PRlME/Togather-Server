ee461401ff2f5d6fcc7b40b32b1f5ef391b4130e
rlaisqls <rlaisqls@gmail.com>
2022-09-17 17:20:52 +0900

    ⚡ :: of 메서드 생성, date 포맷팅

--- src/main/java/com/project/draw/domain/post/presentation/dto/response/PostInfoResponse.java
+++ src/main/java/com/project/draw/domain/post/presentation/dto/response/PostInfoResponse.java
@@ -1,10 +1,11 @@
 package com.project.draw.domain.post.presentation.dto.response;
 
+import com.project.draw.domain.post.domain.Post;
 import com.project.draw.domain.post.domain.Tag;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Getter
@@ -12,8 +13,21 @@ import java.util.List;
 public class PostInfoResponse {
     private final String title;
     private final String userName;
-    private final LocalDateTime createdAt;
+    private final String createdAt;
     private final List<Tag> tags;
     private final String content;
     private final String link;
+
+    public static PostInfoResponse of(Post post) {
+        return PostInfoResponse
+                .builder()
+                .title(post.getTitle())
+                .userName(post.getUser().getName())
+                .content(post.getContent())
+                .link(post.getLink())
+                .tags(post.getTags())
+                .createdAt(post.getCreatedAt()
+                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
+                .build();
+    }
 }
