ecc3543c0ab609a887c8f89230a83228e70fb82a
rlaisqls <rlaisqls@gmail.com>
2022-09-17 21:06:03 +0900

    ⚡ :: 채팅방 조회 로직

--- src/main/java/com/project/draw/domain/chat/service/QueryMyRoomListService.java
+++ src/main/java/com/project/draw/domain/chat/service/QueryMyRoomListService.java
@@ -2,20 +2,34 @@ package com.project.draw.domain.chat.service;
 
 import com.project.draw.domain.chat.domain.enums.RoomType;
 import com.project.draw.domain.chat.domain.repository.RoomRepository;
-import com.project.draw.domain.chat.presentation.dto.response.QueryChatListResponse;
+import com.project.draw.domain.chat.presentation.dto.response.QueryRoomListResponse;
+import com.project.draw.domain.chat.presentation.dto.response.RoomResponse;
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class QueryMyRoomListService {
 
     private final RoomRepository roomRepository;
+    private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
-    public QueryChatListResponse execute(RoomType roomType) {
-        return null;
+    public QueryRoomListResponse execute(RoomType roomType) {
+
+        User user = userFacade.getCurrentUser();
+
+        return new QueryRoomListResponse(
+                roomRepository.findByRoomTypeAndRoomUsers_user(roomType, user)
+                        .stream()
+                        .map(room -> RoomResponse.of(user, room))
+                        .collect(Collectors.toList())
+        );
     }
 
 }
