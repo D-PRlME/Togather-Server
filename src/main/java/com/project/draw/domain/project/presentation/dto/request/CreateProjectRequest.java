6de83b2d6b175b61c3b8d091134e391b234a210b
rlaisqls <rlaisqls@gmail.com>
2022-09-17 18:11:30 +0900

    📝 :: CreateProjectService

---
+++ src/main/java/com/project/draw/domain/project/presentation/dto/request/CreateProjectRequest.java
@@ -0,0 +1,20 @@
+package com.project.draw.domain.project.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class CreateProjectRequest {
+
+    @NotBlank(message = "name은 blank를 허용하지 않습니다.")
+    private String name;
+
+    @NotBlank(message = " projectLogoImage는 blank를 허용하지 않습니다.")
+    private String projectLogoImage;
+
+    @NotBlank(message = "description은 blank를 허용하지 않습니다.")
+    private String description;
+}
