81a343454efd1288999e3564ec74173ee28f44ac
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 19:55:48 +0900

    ⚡ :: deleteAllByPost 추가
    cascade 대체

--- src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
+++ src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
@@ -2,8 +2,11 @@ package com.project.draw.domain.post.domain.repository;
 
 import com.project.draw.domain.post.domain.Like;
 import com.project.draw.domain.post.domain.LikeUserId;
+import com.project.draw.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, LikeUserId> {
+import java.util.Optional;
 
+public interface LikeRepository extends JpaRepository<Like, LikeUserId> {
+    Optional<Like> deleteAllByPost(Post post);
 }
