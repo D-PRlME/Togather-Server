950ce0f09ae9123f946e30fdf9299080132c227b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 11:53:21 +0900

    📝 :: DeleteLikeService

---
+++ src/main/java/com/project/draw/domain/post/service/DeleteLikeService.java
@@ -0,0 +1,28 @@
+package com.project.draw.domain.post.service;
+
+import com.project.draw.domain.post.domain.LikeUserId;
+import com.project.draw.domain.post.domain.Post;
+import com.project.draw.domain.post.domain.repository.LikeRepository;
+import com.project.draw.domain.post.facade.PostFacade;
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteLikeService {
+    private final LikeRepository likeRepository;
+    private final UserFacade userFacade;
+    private final PostFacade postFacade;
+
+    public void execute(Long id) {
+        User user = userFacade.getCurrentUser();
+        Post post = postFacade.getPostById(id);
+
+        likeRepository.deleteById(LikeUserId.builder()
+                .user(user.getId())
+                .post(post.getId())
+                .build());
+    }
+}
