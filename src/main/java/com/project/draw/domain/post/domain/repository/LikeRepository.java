a6b0bfd43ee48c339e5a43892a3410f5f7957cba
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 11:26:36 +0900

    📝 :: LikeRepository

---
+++ src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.project.draw.domain.post.domain.repository;
+
+import com.project.draw.domain.post.domain.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    
+}
