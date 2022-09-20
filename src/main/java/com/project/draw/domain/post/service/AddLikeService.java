b0eda3da64757d443628f36774c5d01d9972bb0e
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 11:40:03 +0900

    📝 :: AddLikeService

---
+++ src/main/java/com/project/draw/domain/post/service/AddLikeService.java
@@ -0,0 +1,28 @@
+package com.project.draw.domain.post.service;
+
+import com.project.draw.domain.post.domain.Like;
+import com.project.draw.domain.post.domain.Post;
+import com.project.draw.domain.post.domain.repository.LikeRepository;
+import com.project.draw.domain.post.facade.PostFacade;
+import com.project.draw.domain.user.domain.User;
+import com.project.draw.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AddLikeService {
+    private final UserFacade userFacade;
+    private final PostFacade postFacade;
+    private final LikeRepository likeRepository;
+
+    public void execute(Long id) {
+        Post post = postFacade.getPostById(id);
+        User user = userFacade.getCurrentUser();
+        post.addLike(likeRepository.save(Like
+                .builder()
+                .user(user)
+                .post(post)
+                .build()));
+    }
+}
