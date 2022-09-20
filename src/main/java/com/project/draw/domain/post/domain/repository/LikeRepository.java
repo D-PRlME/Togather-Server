4fe1649715977345772fd19001a39d393c608582
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 11:52:58 +0900

    ⚡ :: 엔티티 타입 Long에서 LikeUserId로 변경

--- src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
+++ src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
@@ -1,8 +1,9 @@
 package com.project.draw.domain.post.domain.repository;
 
 import com.project.draw.domain.post.domain.Like;
+import com.project.draw.domain.post.domain.LikeUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, LikeUserId> {
 
 }
