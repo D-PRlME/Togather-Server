0909ef1d1f0a57d621f001069660bc9bfc8f28ab
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 20:08:11 +0900

    ♻️ :: Jpa -> Crud 로 변경

--- src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
+++ src/main/java/com/project/draw/domain/post/domain/repository/LikeRepository.java
@@ -3,10 +3,8 @@ package com.project.draw.domain.post.domain.repository;
 import com.project.draw.domain.post.domain.Like;
 import com.project.draw.domain.post.domain.LikeUserId;
 import com.project.draw.domain.post.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface LikeRepository extends JpaRepository<Like, LikeUserId> {
-    Optional<Like> deleteAllByPost(Post post);
+public interface LikeRepository extends CrudRepository<Like, LikeUserId> {
+    void deleteAllByPost(Post post);
 }
