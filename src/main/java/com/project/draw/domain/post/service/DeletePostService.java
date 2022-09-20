d9f72ccf320258dc976e2492e4bcc60e4c0c6edb
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-20 19:57:57 +0900

    ⚡ :: Post 삭제시 좋아요 전체삭제

--- src/main/java/com/project/draw/domain/post/service/DeletePostService.java
+++ src/main/java/com/project/draw/domain/post/service/DeletePostService.java
@@ -1,6 +1,7 @@
 package com.project.draw.domain.post.service;
 
 import com.project.draw.domain.post.domain.Post;
+import com.project.draw.domain.post.domain.repository.LikeRepository;
 import com.project.draw.domain.post.domain.repository.PostRepository;
 import com.project.draw.domain.post.facade.PostFacade;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ public class DeletePostService {
     private final PostFacade postFacade;
     private final PostRepository postRepository;
 
+    private final LikeRepository likeRepository;
+
     public void execute(Long id) {
 
         Post post = postFacade.getPostById(id);
@@ -19,5 +22,7 @@ public class DeletePostService {
         postFacade.checkUser(post);
 
         postRepository.delete(postFacade.getPostById(id));
+
+        likeRepository.deleteAllByPost(post);
     }
 }
