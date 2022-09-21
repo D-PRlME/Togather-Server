8a997f6d4c8b8371776b3d53a8d471941cef8439
rlaisqls <rlaisqls@gmail.com>
2022-09-21 22:53:01 +0900

    📝 :: QueryMyInvitationService

--- src/main/java/com/project/draw/domain/project/presentation/ProjectController.java
+++ src/main/java/com/project/draw/domain/project/presentation/ProjectController.java
@@ -1,12 +1,20 @@
 package com.project.draw.domain.project.presentation;
 
+import com.project.draw.domain.project.presentation.dto.request.AcceptInviteRequest;
 import com.project.draw.domain.project.presentation.dto.request.CreateProjectRequest;
+import com.project.draw.domain.project.presentation.dto.request.InviteUserRequest;
 import com.project.draw.domain.project.presentation.dto.request.LeaveProjectRequest;
+import com.project.draw.domain.project.presentation.dto.response.QueryMyInvitationResponse;
+import com.project.draw.domain.project.service.AcceptInviteService;
 import com.project.draw.domain.project.service.CreateProjectService;
+import com.project.draw.domain.project.service.InviteUserService;
 import com.project.draw.domain.project.service.LeaveProjectService;
+import com.project.draw.domain.project.service.QueryMyInvitationService;
 import com.project.draw.domain.project.service.QueryProjectInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +29,9 @@ import javax.validation.Valid;
 public class ProjectController {
 
     private final CreateProjectService createProjectService;
+    private final InviteUserService inviteUserService;
+    private final QueryMyInvitationService queryMyInvitationService;
+    private final AcceptInviteService acceptInviteService;
     private final LeaveProjectService leaveProjectService;
     private final QueryProjectInfoService queryProjectInfoService;
 
@@ -28,6 +39,23 @@ public class ProjectController {
     @PostMapping
     public void createProject(@RequestBody @Valid CreateProjectRequest request) {
         createProjectService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/invite")
+    public void inviteUser(@RequestBody @Valid InviteUserRequest request) {
+        inviteUserService.execute(request);
+    }
+
+    @GetMapping("/invite")
+    public QueryMyInvitationResponse queryMyInvitation() {
+        return queryMyInvitationService.execute();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/invite")
+    public void acceptInvite(@RequestBody @Valid AcceptInviteRequest request) {
+        acceptInviteService.execute(request);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
