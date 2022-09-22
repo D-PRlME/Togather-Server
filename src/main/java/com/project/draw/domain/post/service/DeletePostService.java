b011c3dd6f5bba9c110c446d937cc5142282ecda
rlaisqls <rlaisqls@gmail.com>
2022-09-22 22:36:52 +0900

    ⚡ :: CASCADE 설정

--- src/main/java/com/project/draw/domain/post/service/DeletePostService.java
+++ src/main/java/com/project/draw/domain/post/service/DeletePostService.java
@@ -1,7 +1,6 @@
 package com.project.draw.domain.post.service;
 
 import com.project.draw.domain.post.domain.Post;
-import com.project.draw.domain.post.domain.repository.LikeRepository;
 import com.project.draw.domain.post.domain.repository.PostRepository;
 import com.project.draw.domain.post.facade.PostFacade;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +9,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class DeletePostService {
+
     private final PostFacade postFacade;
     private final PostRepository postRepository;
-
-    private final LikeRepository likeRepository;
 
     public void execute(Long id) {
 
@@ -22,7 +20,5 @@ public class DeletePostService {
         postFacade.checkUser(post);
 
         postRepository.delete(postFacade.getPostById(id));
-
-        likeRepository.deleteAllByPost(post);
     }
 }
