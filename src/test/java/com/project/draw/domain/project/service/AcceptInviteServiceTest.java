dfbcabcf040c1895665324a78a64b8b197c7ebd2
rlaisqls <rlaisqls@gmail.com>
2022-09-22 13:33:55 +0900

    🧪 :: 테스트 경량화

--- src/test/java/com/project/draw/domain/project/service/AcceptInviteServiceTest.java
+++ src/test/java/com/project/draw/domain/project/service/AcceptInviteServiceTest.java
@@ -7,16 +7,13 @@ import com.project.draw.domain.project.exception.UserNotInvitedException;
 import com.project.draw.domain.project.presentation.dto.request.AcceptInviteRequest;
 import com.project.draw.domain.user.domain.User;
 import com.project.draw.domain.user.facade.UserFacade;
-import com.project.draw.util.ProjectBuilder;
-import com.project.draw.util.ProjectInvitationBuilder;
-import com.project.draw.util.UserBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,57 +21,82 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 @ExtendWith(MockitoExtension.class)
 class AcceptInviteServiceTest {
 
     @Mock
-    private ProjectInvitationRepository projectInvitationRepository;
+    private UserFacade userFacade;
 
     @Mock
-    private UserFacade userFacade;
+    private ProjectInvitationRepository projectInvitationRepository;
 
     @InjectMocks
     private AcceptInviteService service;
 
-    static private User user;
-    static private ProjectInvitation projectInvitation;
-
-    @BeforeAll
-    static void setUp() {
-        user = UserBuilder.build();
-        Project project = ProjectBuilder.build(user);
-        projectInvitation = ProjectInvitationBuilder.build(project, user);
-    }
-
     @Test
-    public void 프로젝트_초대_수락 () {
+    public void 프로젝트_초대_수락() {
         //given
+        User user = User.builder().build();
+        Project project = Project.builder().build();
+        setField(project, "projectUsers", new ArrayList<>());
+
+        ProjectInvitation projectInvitation = ProjectInvitation
+                .builder()
+                .project(project)
+                .build();
+
+        given(userFacade.getCurrentUser()).willReturn(user);
+        given(projectInvitationRepository.findById(any())).willReturn(Optional.of(projectInvitation));
+
         AcceptInviteRequest request = new AcceptInviteRequest();
-        given(userFacade.getCurrentUser())
-                .willReturn(user);
-        given(projectInvitationRepository.findById(any()))
-                .willReturn(Optional.of(projectInvitation));
+        setField(request, "isAccept", true);
 
         //when
         service.execute(request);
 
         //then
-        then(projectInvitationRepository).should(times(1)).findById(any());
-        verify(projectInvitation.getProject(), times(1)).addProjectUsers(user);
         then(projectInvitationRepository).should(times(1)).delete(projectInvitation);
     }
 
+    @Test
+    public void 프로젝트_초대_거절() {
+        //given
+        User user = User.builder().build();
+        Project project = Project.builder().build();
+        setField(project, "projectUsers", new ArrayList<>());
+
+        ProjectInvitation projectInvitation = ProjectInvitation
+                .builder()
+                .project(project)
+                .build();
+
+        given(userFacade.getCurrentUser()).willReturn(user);
+        given(projectInvitationRepository.findById(any())).willReturn(Optional.of(projectInvitation));
+
+        AcceptInviteRequest request = new AcceptInviteRequest();
+        setField(request, "isAccept", false);
+
+        //when
+        service.execute(request);
+
+        //then
+        then(projectInvitationRepository).should(times(1)).delete(projectInvitation);
+    }
 
     @Test
-    public void 프로젝트_초대_없음_실패 () {
+    public void 프로젝트_초대_수락_실패() {
+
         //given
+        User user = User.builder().build();
+        Project project = Project.builder().build();
+        setField(project, "projectUsers", new ArrayList<>());
+
+        given(userFacade.getCurrentUser()).willReturn(user);
+        given(projectInvitationRepository.findById(any())).willReturn(null);
+
         AcceptInviteRequest request = new AcceptInviteRequest();
-        given(userFacade.getCurrentUser())
-                .willReturn(user);
-        given(projectInvitationRepository.findById(any()))
-                .willReturn(Optional.empty());
 
         //when then
         assertThrows(UserNotInvitedException.class, () -> service.execute(request));
