30f99629fc1f56020e3298d7e8d04ec48b3856bb
rlaisqls <rlaisqls@gmail.com>
2022-09-25 12:56:34 +0900

    📝 :: RegexpProperty

---
+++ src/main/java/com/project/draw/global/util/RegexpProperty.java
@@ -0,0 +1,12 @@
+package com.project.draw.global.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RegexpProperty {
+
+    public static final String EMAIL = "^.+@dsm.hs.kr$|^.+@bssm.hs.kr$|^.+@gsm.hs.kr$|^.+@dgsw.hs.kr$";
+
+    public static final String PASSWORD = "^.+@dsm.hs.kr$|^.+@bssm.hs.kr$|^.+@gsm.hs.kr$|^.+@dgsw.hs.kr$";
+
+}
