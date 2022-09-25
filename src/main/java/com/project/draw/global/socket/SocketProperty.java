3e37ddbe54797455190becab5046bd7cabaf5a5d
rlaisqls <rlaisqls@gmail.com>
2022-09-25 23:08:04 +0900

    ⚡ :: joinEvent 추가

--- src/main/java/com/project/draw/global/socket/SocketProperty.java
+++ src/main/java/com/project/draw/global/socket/SocketProperty.java
@@ -2,7 +2,13 @@ package com.project.draw.global.socket;
 
 public class SocketProperty {
 
+    //request
     public static final String CHAT = "chat";
+
+    public static final String JOIN = "join";
+
+    //response
+    //public static final String CHAT = "chat";
 
     public static final String ROOM = "room";
 
