741f232e6a4eec74a4bb0b7bf51d6458108d9b21
rlaisqls <rlaisqls@gmail.com>
2022-09-25 22:48:24 +0900

    ♻️ :: 안쓰는 메서드 삭제

--- src/main/java/com/project/draw/domain/chat/domain/repository/RoomRepository.java
+++ src/main/java/com/project/draw/domain/chat/domain/repository/RoomRepository.java
@@ -1,13 +1,7 @@
 package com.project.draw.domain.chat.domain.repository;
 
 import com.project.draw.domain.chat.domain.Room;
-import com.project.draw.domain.chat.domain.enums.RoomType;
-import com.project.draw.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface RoomRepository extends CrudRepository<Room, Long> {
-    Optional<Room> findByIdAndRoomUsers_user(Long id, User user);
-    Optional<Room> findByRoomTypeAndRoomUsers_user(RoomType roomType, User user);
 }
