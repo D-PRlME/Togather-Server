91532c62efd2e650606ba31ca49ae7522edf3b22
rlaisqls <rlaisqls@gmail.com>
2022-09-25 13:09:15 +0900

    🐛 :: 비밀번호 정규식 수정
    복붙을 조심합시다

--- src/main/java/com/project/draw/global/util/RegexpProperty.java
+++ src/main/java/com/project/draw/global/util/RegexpProperty.java
@@ -7,6 +7,6 @@ public class RegexpProperty {
 
     public static final String EMAIL = "^.+@dsm.hs.kr$|^.+@bssm.hs.kr$|^.+@gsm.hs.kr$|^.+@dgsw.hs.kr$";
 
-    public static final String PASSWORD = "^.+@dsm.hs.kr$|^.+@bssm.hs.kr$|^.+@gsm.hs.kr$|^.+@dgsw.hs.kr$";
+    public static final String PASSWORD = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,30}$";
 
 }
