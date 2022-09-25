eefdda7e648d6c7288a2f989137eb3d3a3685964
rlaisqls <rlaisqls@gmail.com>
2022-09-25 23:08:51 +0900

    ⚡ :: room_key 추가

--- src/main/java/com/project/draw/global/socket/security/ClientProperty.java
+++ src/main/java/com/project/draw/global/socket/security/ClientProperty.java
@@ -3,5 +3,7 @@ package com.project.draw.global.socket.security;
 public class ClientProperty {
 
     public static final String USER_KEY = "user_key";
+    
+    public static final String ROOM_KEY = "room_key";
 
 }
