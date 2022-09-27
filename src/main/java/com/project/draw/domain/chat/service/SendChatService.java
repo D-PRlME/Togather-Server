0aba2008fe3ab9e0da03b9d8e34d080a71348b57
rlaisqls <rlaisqls@gmail.com>
2022-09-27 21:12:42 +0900

    ⚡ :: 소켓 수신 유저 읽음여부 설정

--- src/main/java/com/project/draw/domain/chat/service/SendChatService.java
+++ src/main/java/com/project/draw/domain/chat/service/SendChatService.java
@@ -4,13 +4,16 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.project.draw.domain.chat.domain.Chat;
 import com.project.draw.domain.chat.domain.Room;
+import com.project.draw.domain.chat.domain.RoomUser;
 import com.project.draw.domain.chat.domain.repository.ChatRepository;
 import com.project.draw.domain.chat.facade.RoomFacade;
+import com.project.draw.domain.chat.facade.RoomUserFacade;
 import com.project.draw.domain.chat.presentation.dto.request.SendChatRequest;
 import com.project.draw.domain.chat.presentation.dto.response.ChatResponse;
 import com.project.draw.domain.user.domain.User;
 import com.project.draw.domain.user.facade.UserFacade;
 import com.project.draw.global.socket.SocketProperty;
+import com.project.draw.global.socket.util.SocketUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +24,7 @@ public class SendChatService {
 
     private final ChatRepository chatRepository;
     private final UserFacade userFacade;
+    private final RoomUserFacade roomUserFacade;
     private final RoomFacade roomFacade;
 
     @Transactional
@@ -28,6 +32,8 @@ public class SendChatService {
 
         User user = userFacade.getCurrentUser(socketIOClient);
         Room room = roomFacade.getCurrentRoom(socketIOClient);
+
+        RoomUser roomUser = roomUserFacade.getById(room.getId(), user.getId());
 
         Chat chat = chatRepository.save(Chat
                 .builder()
@@ -37,8 +43,17 @@ public class SendChatService {
                 .build());
 
         room.updateLastMessage(chat);
+        roomUser.updateLastReadTime();
 
-        socketIOServer.getRoomOperations(room.getId().toString())
-                .sendEvent(SocketProperty.CHAT, ChatResponse.of(chat));
+        socketIOServer
+                .getRoomOperations(room.getId().toString())
+                .getClients()
+                .forEach(client -> {
+                    client.sendEvent(SocketProperty.CHAT, ChatResponse.of(chat));
+                    RoomUser clientRoomUser = roomUserFacade
+                            .getById(room.getId(), SocketUtil.getUserId(client));
+                    clientRoomUser.updateLastReadTime();
+                });
+
     }
 }
