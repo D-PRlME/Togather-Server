5a1450d0c37946f090771ab064799aa521ea5e35
rlaisqls <rlaisqls@gmail.com>
2022-10-06 22:30:45 +0900

    ♻️ :: static 변수명 대문자로

--- src/main/java/com/project/draw/global/util/auth/service/QueryGoogleAuthLinkService.java
+++ src/main/java/com/project/draw/global/util/auth/service/QueryGoogleAuthLinkService.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryGoogleAuthLinkService {
 
-    private static final String url = "%s?client_id=%s&redirect_uri=%s&response_type=code"
+    private static final String URL = "%s?client_id=%s&redirect_uri=%s&response_type=code"
             + "&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 
     private final AuthProperties authProperties;
 
     public String execute() {
-        return String.format(url, //이 형식으로 아래 것들을 짜준다 ~
+        return String.format(URL,
                 authProperties.getBaseUrl(),
                 authProperties.getClientId(),
                 authProperties.getRedirectUrl());
