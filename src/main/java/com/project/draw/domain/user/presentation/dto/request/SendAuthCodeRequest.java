1d3c0220bb27742ddb923bd395689373532c7d08
김은빈 <rlaisqls@gmail.com>
2022-10-24 21:31:08 +0900

    🐛 :: NotNull 유효성 검사

--- src/main/java/com/project/draw/domain/user/presentation/dto/request/SendAuthCodeRequest.java
+++ src/main/java/com/project/draw/domain/user/presentation/dto/request/SendAuthCodeRequest.java
@@ -4,6 +4,7 @@ import com.project.draw.global.util.RegexpProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -11,5 +12,6 @@ import javax.validation.constraints.Pattern;
 public class SendAuthCodeRequest {
 
     @Pattern(regexp = RegexpProperty.EMAIL)
+    @NotBlank(message = "email은 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
     private String email;
 }
